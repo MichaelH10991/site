0c48a3ed93adfd1b59d0efa4db87bd8761f45d24
Michael Humphries <michaelhumphries1997@gmail.com>
2019-04-29 19:55:47 +0100

    making comment object

--- public/js/comment-handler.js
+++ public/js/comment-handler.js
@@ -1,6 +1,38 @@
-var comment_array = []
+let comment_array = []
+
+// class for creating comments
+class Comment {
+  constructor(date, text){
+    this.date = date;
+    this.text = text;
+  }
+}
 
 function submit() {
-  var comment = document.getElementById("comment-box").value
-  comment_array.push(comment)
+  comment_array.push(getComment())
+  let comment_container = document.createElement("DIV")
+  comment_container.innerHTML = getComment()
+  document.getElementById("jumbo").appendChild(comment_container)
+}
+
+function getComment() {
+  let date = getDate()
+  let text = document.getElementById("comment-box").value
+  comment = new Comment(date, text)
+  return `You said "${comment.text}" on ${comment.date}`
+}
+
+// a booky class that creates a custom date
+class CustomDate{
+  constructor(){
+    this.options = {
+      weekday: 'long', year: 'numeric', month: 'long', day: 'numeric', hour: 'numeric', minute: 'numeric'
+    }
+    this.today = new Date();
+  }
+}
+
+function getDate(){
+  let date = new CustomDate()
+  return date.today.toLocaleDateString('en-US', date.options)
 }
